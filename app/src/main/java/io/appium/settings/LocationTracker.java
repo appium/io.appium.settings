4cec677c0d157125597b37a08f4ba91859608bf0
Mykola Mokhnach <mokhnach@gmail.com>
2019-01-26 18:47:18 +0100

    Add verification of location tracker state (#36)

--- app/src/main/java/io/appium/settings/LocationTracker.java
+++ app/src/main/java/io/appium/settings/LocationTracker.java
@@ -16,7 +16,6 @@
 
 package io.appium.settings;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
@@ -31,6 +30,9 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import io.appium.settings.helpers.PlayServicesHelpers;
 
 import static android.content.Context.LOCATION_SERVICE;
@@ -42,13 +44,16 @@ public class LocationTracker implements GoogleApiClient.ConnectionCallbacks,
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
     private static final long LOCATION_UPDATES_INTERVAL = 1000 * 60; // 1 minute
     private static final long FAST_INTERVAL = 5000; // 5 seconds
+    private static final long GOOGLE_API_CONNECT_TIMEOUT = 5000;
 
-    private LocationManager mLocationManager;
-    private GoogleApiClient mGoogleApiClient;
+    private CountDownLatch googleApiStartupLatch = new CountDownLatch(1);
+    private volatile LocationManager mLocationManager;
+    private volatile GoogleApiClient mGoogleApiClient;
     private volatile Location mLocation;
-    private String locationProvider;
+    private volatile boolean isStarted = false;
+    private String mLocationProvider;
+    private volatile Boolean hasAccessToLocationServices = null;
 
-    @SuppressLint("StaticFieldLeak")
     private static LocationTracker instance = null;
 
     private LocationTracker() {
@@ -64,6 +69,7 @@ public class LocationTracker implements GoogleApiClient.ConnectionCallbacks,
     @Override
     public void onLocationChanged(Location location) {
         if (location != null) {
+            Log.d(TAG, "Got an updated location");
             mLocation = location;
         }
     }
@@ -82,41 +88,8 @@ public class LocationTracker implements GoogleApiClient.ConnectionCallbacks,
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        startLocationUpdatesWithPlayServices();
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-    }
-
-    void start(Context mContext) {
-        if (PlayServicesHelpers.isAvailable(mContext)) {
-            Log.i(TAG, "Configuring location provider for Google Play Services");
-            stopLocationUpdatesWithPlayServices();
-            mGoogleApiClient = new GoogleApiClient.Builder(mContext)
-                    .addApi(LocationServices.API)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .build();
-            mGoogleApiClient.connect();
-        } else {
-            Log.i(TAG, "Configuring the default Android location provider");
-            stopLocationUpdatesWithoutPlayServices();
-            mLocationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
-            if (mLocationManager == null) {
-                Log.e(TAG, "Cannot retrieve the location manager");
-                return;
-            }
-            startLocationUpdatesWithoutPlayServices();
-        }
-    }
-
-    private void startLocationUpdatesWithPlayServices() {
         if (mGoogleApiClient == null) {
+            googleApiStartupLatch.countDown();
             return;
         }
 
@@ -128,21 +101,83 @@ public class LocationTracker implements GoogleApiClient.ConnectionCallbacks,
             //noinspection deprecation
             LocationServices.FusedLocationApi
                     .requestLocationUpdates(mGoogleApiClient, locationRequest, this);
+            Log.d(TAG, "Google Play Services location provider is connected");
+            hasAccessToLocationServices = true;
         } catch (SecurityException e) {
-            Log.e(TAG, "Appium Settings has no access to location permission");
+            Log.e(TAG, "Appium Settings has no access to location permission", e);
+            hasAccessToLocationServices = false;
+            stopLocationUpdatesWithPlayServices();
+        }
+        googleApiStartupLatch.countDown();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        if (mGoogleApiClient == null) {
+            googleApiStartupLatch.countDown();
+            return;
+        }
+
+        Log.e(TAG, String.format("Google Play Services location provider has failed to connect (code %s)",
+                connectionResult.toString()));
+        stopLocationUpdatesWithPlayServices();
+        googleApiStartupLatch.countDown();
+    }
+
+    synchronized void start(Context context, long googleApiConnectTimeout, boolean force) {
+        if (isStarted && !force) {
+            return;
+        }
+        isStarted = true;
+
+        boolean isPlayServicesAvailable = PlayServicesHelpers.isAvailable(context);
+        if (isPlayServicesAvailable) {
+            Log.d(TAG, "Configuring location provider for Google Play Services");
+            stopLocationUpdatesWithPlayServices();
+            mGoogleApiClient = new GoogleApiClient.Builder(context)
+                    .addApi(LocationServices.API)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+            googleApiStartupLatch = new CountDownLatch(1);
+            mGoogleApiClient.connect();
+            if (googleApiConnectTimeout > 0) {
+                try {
+                    googleApiStartupLatch.await(googleApiConnectTimeout, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException e) {
+                    Log.w(TAG, e);
+                }
+            }
+        }
+
+        if (!isPlayServicesAvailable || mGoogleApiClient == null) {
+            Log.d(TAG, "Configuring the default Android location provider");
+            stopLocationUpdatesWithoutPlayServices();
+            mLocationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+            if (mLocationManager == null) {
+                Log.e(TAG, "Cannot retrieve the location manager");
+                return;
+            }
+            startLocationUpdatesWithoutPlayServices();
         }
     }
 
     private void stopLocationUpdatesWithPlayServices() {
-        if (mGoogleApiClient == null || !mGoogleApiClient.isConnected()) {
+        if (mGoogleApiClient == null) {
             return;
         }
 
-        Log.i(TAG, "Stopping Google Play Services location provider");
-        //noinspection deprecation
-        LocationServices.FusedLocationApi
-                .removeLocationUpdates(mGoogleApiClient, this);
-        mGoogleApiClient.disconnect();
+        Log.d(TAG, "Stopping Google Play Services location provider");
+        if (mGoogleApiClient.isConnected()) {
+            //noinspection deprecation
+            LocationServices.FusedLocationApi
+                    .removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
         mGoogleApiClient = null;
     }
 
@@ -156,7 +191,7 @@ public class LocationTracker implements GoogleApiClient.ConnectionCallbacks,
         boolean isNetworkEnabled = mLocationManager
                 .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
         if (!isGPSEnabled && !isNetworkEnabled) {
-            Log.e(TAG, "Both Coarse and GPS providers are disabled");
+            Log.e(TAG, "Both COARSE and GPS location providers are disabled");
             return;
         }
 
@@ -166,11 +201,12 @@ public class LocationTracker implements GoogleApiClient.ConnectionCallbacks,
                         LocationManager.GPS_PROVIDER,
                         LOCATION_UPDATES_INTERVAL,
                         MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-                locationProvider = LocationManager.GPS_PROVIDER;
-                Log.d(TAG, "GPS enabled. Getting FINE location");
+                mLocationProvider = LocationManager.GPS_PROVIDER;
+                Log.d(TAG, "GPS location provider is enabled. Getting FINE location");
+                hasAccessToLocationServices = true;
                 return;
             } catch (SecurityException e) {
-                Log.e(TAG, "Appium Settings has no access to FINE location permission");
+                Log.e(TAG, "Appium Settings has no access to FINE location permission", e);
             }
         }
         try {
@@ -178,10 +214,12 @@ public class LocationTracker implements GoogleApiClient.ConnectionCallbacks,
                     LocationManager.NETWORK_PROVIDER,
                     LOCATION_UPDATES_INTERVAL,
                     MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-            locationProvider = LocationManager.NETWORK_PROVIDER;
-            Log.d(TAG, "Network enabled. Getting COARSE location");
+            mLocationProvider = LocationManager.NETWORK_PROVIDER;
+            Log.d(TAG, "NETWORK location provider is enabled. Getting COARSE location");
+            hasAccessToLocationServices = true;
         } catch (SecurityException e) {
-            Log.e(TAG, "Appium Settings has no access to COARSE location permission");
+            hasAccessToLocationServices = false;
+            Log.e(TAG, "Appium Settings has no access to COARSE location permission", e);
         }
     }
 
@@ -190,29 +228,58 @@ public class LocationTracker implements GoogleApiClient.ConnectionCallbacks,
             return;
         }
 
-        Log.i(TAG, "Stopping Android location provider");
+        Log.d(TAG, "Stopping Android location provider");
         mLocationManager.removeUpdates(this);
         mLocationManager = null;
     }
-    
+
+    private boolean isPlayServicesConnected() {
+        return mGoogleApiClient != null && mGoogleApiClient.isConnected();
+    }
+
+    private boolean isLocationManagerConnected() {
+        return mLocationManager != null && mLocationProvider != null;
+    }
+
     @Nullable
-    public Location getLocation() {
-        if (mLocation == null) {
-            if (mGoogleApiClient != null) {
-                if (mGoogleApiClient.isConnected()) {
-                    try {
-                        //noinspection deprecation
-                        mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-                    } catch (SecurityException e) {
-                        Log.e(TAG, "Appium Settings has no access to location permission");
-                    }
+    public synchronized Location getLocation(Context context) {
+        if (mLocation != null) {
+            return mLocation;
+        }
+
+        // Make sure the service has been started
+        final boolean forceStart = hasAccessToLocationServices != null && !hasAccessToLocationServices;
+        start(context, GOOGLE_API_CONNECT_TIMEOUT, forceStart);
+
+        if (isPlayServicesConnected()) {
+            // Make sure the fallback is removed after play services connection succeeds
+            if (isLocationManagerConnected()) {
+                stopLocationUpdatesWithoutPlayServices();
+            }
+            try {
+                //noinspection deprecation
+                mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                hasAccessToLocationServices = true;
+            } catch (SecurityException e) {
+                hasAccessToLocationServices = false;
+                Log.e(TAG, "Appium Settings has no access to location permission", e);
+            }
+        } else if (isLocationManagerConnected() || mGoogleApiClient != null) {
+            // Try fallback to the default provider if Google API is available but is still not connected
+            if (mGoogleApiClient != null && !isLocationManagerConnected()) {
+                mLocationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+                if (mLocationManager != null) {
+                    startLocationUpdatesWithoutPlayServices();
                 }
-            } else if (mLocationManager != null && locationProvider != null) {
+            }
+            if (mLocationManager != null && mLocationProvider != null) {
                 try {
-                    mLocation = mLocationManager.getLastKnownLocation(locationProvider);
+                    mLocation = mLocationManager.getLastKnownLocation(mLocationProvider);
+                    hasAccessToLocationServices = true;
                 } catch (SecurityException e) {
+                    hasAccessToLocationServices = false;
                     Log.e(TAG, String.format("Appium Settings has no access to %s location permission",
-                            locationProvider));
+                            mLocationProvider), e);
                 }
             }
         }
