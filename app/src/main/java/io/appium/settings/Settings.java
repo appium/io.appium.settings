13948af97471bf0cd74b55625f4d17d8b1f0738b
Mykola Mokhnach <mokhnach@gmail.com>
2021-11-30 16:58:06 +0100

    Add receiver

--- app/src/main/java/io/appium/settings/Settings.java
+++ app/src/main/java/io/appium/settings/Settings.java
@@ -34,6 +34,7 @@ import io.appium.settings.receivers.DataConnectionSettingReceiver;
 import io.appium.settings.receivers.HasAction;
 import io.appium.settings.receivers.LocaleSettingReceiver;
 import io.appium.settings.receivers.LocationInfoReceiver;
+import io.appium.settings.receivers.MediaScannerReceiver;
 import io.appium.settings.receivers.NotificationsReceiver;
 import io.appium.settings.receivers.SmsReader;
 import io.appium.settings.receivers.UnpairBluetoothDevicesReceiver;
@@ -61,6 +62,7 @@ public class Settings extends Activity {
         receiverClasses.add(UnpairBluetoothDevicesReceiver.class);
         receiverClasses.add(NotificationsReceiver.class);
         receiverClasses.add(SmsReader.class);
+        receiverClasses.add(MediaScannerReceiver.class);
         registerSettingsReceivers(receiverClasses);
 
         // https://developer.android.com/about/versions/oreo/background-location-limits
