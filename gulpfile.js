23b4bfe2fa7f21a9c431cd147c52e15948c1abd5
Mykola Mokhnach <mokhnach@gmail.com>
2022-01-20 16:50:02 +0100

    refactor!: Switch the package to use Appium2 modules for development

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 "use strict";
 
 const gulp = require('gulp');
-const boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
+const boilerplate = require('@appium/gulp-plugins').boilerplate.use(gulp);
 
 
 boilerplate({
   build: 'io.appium.settings',
+  files: ['index.js', '!gulpfile.js'],
 });
