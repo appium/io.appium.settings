dffc419877f69b224784e7355b1bceae9888bda1
Mykola Mokhnach <mokhnach@gmail.com>
2022-07-10 13:47:04 +0200

    chore: Update locale selection procedure if no matches were found for it

--- app/src/main/java/io/appium/settings/receivers/LocaleSettingReceiver.java
+++ app/src/main/java/io/appium/settings/receivers/LocaleSettingReceiver.java
@@ -26,6 +26,8 @@ import android.util.Log;
 
 import org.apache.commons.lang3.LocaleUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import io.appium.settings.handlers.LocaleSettingHandler;
@@ -54,7 +56,7 @@ public class LocaleSettingReceiver extends BroadcastReceiver implements HasActio
         // Expect https://developer.android.com/reference/java/util/Locale.html#Locale(java.lang.String,%20java.lang.String) format.
         Locale locale = new Locale(language, country);
         String script = intent.getStringExtra(SCRIPT);
-        if (!isBlank(script)) {
+        if (script != null) {
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
                 locale = new Locale.Builder().setLocale(locale).setScript(script).build();
             } else {
@@ -63,13 +65,32 @@ public class LocaleSettingReceiver extends BroadcastReceiver implements HasActio
             }
         }
         if (!LocaleUtils.isAvailableLocale(locale)) {
-            Log.e(TAG, String.format(
-                    "The locale %s is not known. Only the following locales are available: %s",
-                    locale, LocaleUtils.availableLocaleList())
-            );
-            setResultCode(Activity.RESULT_CANCELED);
-            setResultData(String.format("The locale %s is not known", locale));
-            return;
+            List<Locale> approximateMatchesLc = matchLocales(language, country);
+            if (!approximateMatchesLc.isEmpty() && isBlank(script)) {
+                Log.i(TAG, String.format(
+                        "The locale %s is not known. Selecting the closest known one %s instead",
+                        locale, approximateMatchesLc.get(0))
+                );
+                locale = approximateMatchesLc.get(0);
+            } else {
+                List<Locale> approximateMatchesL = matchLocales(language);
+                if (approximateMatchesL.isEmpty()) {
+                    Log.e(TAG, String.format(
+                            "The locale %s is not known. Only the following locales are available: %s",
+                            locale, LocaleUtils.availableLocaleList())
+                    );
+                } else {
+                    Log.e(TAG, String.format(
+                            "The locale %s is not known. " +
+                                    "The following locales are available for the %s language: %s" +
+                                    "The following locales are available altogether: %s",
+                            locale, language, approximateMatchesL, LocaleUtils.availableLocaleList())
+                    );
+                }
+                setResultCode(Activity.RESULT_CANCELED);
+                setResultData(String.format("The locale %s is not known", locale));
+                return;
+            }
         }
 
         new LocaleSettingHandler(context).setLocale(locale);
@@ -81,5 +102,26 @@ public class LocaleSettingReceiver extends BroadcastReceiver implements HasActio
     @Override
     public String getAction() {
         return ACTION;
+    }
+
+    private static List<Locale> matchLocales(String language) {
+        List<Locale> matches = new ArrayList<>();
+        for (Locale locale: LocaleUtils.availableLocaleList()) {
+            if (locale.getLanguage().equalsIgnoreCase(language)) {
+                matches.add(locale);
+            }
+        }
+        return matches;
+    }
+
+    private static List<Locale> matchLocales(String language, String country) {
+        List<Locale> matches = new ArrayList<>();
+        for (Locale locale: LocaleUtils.availableLocaleList()) {
+            if (locale.getLanguage().equalsIgnoreCase(language)
+                    && locale.getCountry().equalsIgnoreCase(country)) {
+                matches.add(locale);
+            }
+        }
+        return matches;
     }
 }
