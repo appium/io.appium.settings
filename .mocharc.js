5c5d8ea3a1f5405617b3187d275cfd7e513842fa
Mykola Mokhnach <mokhnach@gmail.com>
2024-01-08 21:49:58 +0100

    feat: Add javascript wrappers over Settings App APIs

---
+++ .mocharc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  require: ['ts-node/register'],
+  forbidOnly: Boolean(process.env.CI),
+  color: true
+};
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const path = require('path');
+const path = require('node:path');
+const { SettingsApp } = require('./build/lib/client');
+const constants = require('./build/lib/constants');
 
 module.exports = {
-  path: path.resolve(__dirname, 'apks', 'settings_apk-debug.apk')
+  path: path.resolve(__dirname, 'apks', 'settings_apk-debug.apk'),
+  SettingsApp,
+  ...constants,
 };
