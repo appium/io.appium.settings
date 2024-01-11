7020af0c0ec542b2271b5e4685ff9c63a5faa4c2
Mykola Mokhnach <mokhnach@gmail.com>
2024-01-11 15:46:59 +0100

    feat: Make the index typed (#140)

--- index.js
+++
@@ -1,9 +0,0 @@
-const path = require('node:path');
-const { SettingsApp } = require('./build/lib/client');
-const constants = require('./build/lib/constants');
-
-module.exports = {
-  path: path.resolve(__dirname, 'apks', 'settings_apk-debug.apk'),
-  SettingsApp,
-  ...constants,
-};
