958e8be1362bfae75388d622dc36c5169858805f
Mykola Mokhnach <mokhnach@gmail.com>
2026-06-29 17:09:30 +0200

    format

--- lib/commands/utf7.ts
+++ lib/commands/utf7.ts
@@ -124,7 +124,7 @@ function _decode(str: string): string {
   const b = allocateBase64Buffer(str);
   const r: string[] = [];
   // eslint-disable-next-line @stylistic/space-in-parens
-  for (let i = 0; i < b.length; ) {
+  for (let i = 0; i < b.length;) {
     // Calculate charcode from two adjacent bytes.
     r.push(String.fromCharCode((b[i++] << 8) | b[i++]));
   }
